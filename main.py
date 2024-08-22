56053237a248433439ed9d6c0a702320ee28d51b
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2024-08-22 15:53:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
+#https://www.youtube.com/watch?v=iF7osyiDkBY
+#API keys
 import config
-asassaasdasd
+# connection to Alpaca
+from alpaca.trading.client import TradingClient
 
-asdas
+trading_client = TradingClient(config.APCA-API-KEY-ID, config.APCA-API-SECRET-KEY)
+
+print(trading_client.get_account().account_number)
+print(trading_client.get_account().buying_power)
