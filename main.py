fd676181757022c4b4b73774379c059a3ac3ebac
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2024-08-22 15:59:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import config
 # connection to Alpaca
 from alpaca.trading.client import TradingClient
 
-trading_client = TradingClient(config.APCA-API-KEY-ID, config.APCA-API-SECRET-KEY)
+trading_client = TradingClient(config.alpacakey, config.alpacakey_secret)
 
 print(trading_client.get_account().account_number)
 print(trading_client.get_account().buying_power)
