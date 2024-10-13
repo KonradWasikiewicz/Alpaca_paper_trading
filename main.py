73fa068a081f1a37ad7baf203d540f030c847eb1
KonradWasikiewicz <konrad.wasikiewicz@gmail.com>
2024-10-13 17:28:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-# #https://www.youtube.com/watch?v=iF7osyiDkBY
-# #API keys
-# import config
-# # connection to Alpaca
-# from alpaca.trading.client import TradingClient
+#https://www.youtube.com/watch?v=iF7osyiDkBY
+#API keys
+import config
+# connection to Alpaca
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
 
-# trading_client = TradingClient(config.alpacakey, config.alpacakey_secret)
+trading_client = TradingClient(config.alpacakey, config.alpacakey_secret)
 
-# print(trading_client.get_account().account_number)
-# print(trading_client.get_account().buying_power)
-
-
-from alpaca.data import StockHistoricalDataClient, StockTradesRequest
-from datetime import datetime
-
-data_client = StockHistoricalDataClient("PKQGC4V9XTMJGSD9HF29","2HRWei8pngjyCJmQeceFowiarg6Ktrd2zG9wQ0Qt")
-
-request_params = StockTradesRequest(
-    symbol_or_symbols = "PLTR",
-    start = datetime(2024, 1, 30, 14, 30),
-    end = datetime(2024, 1, 30, 14, 45) 
+market_order_data = MarketOrderRequest(
+    symbol="SPY",
+    qty= 1,
+    side=OrderSide.BUY,
+    time_in_force=TimeInForce.DAY
 )
 
-trades = data_client.get_stock_trades(request_params)
+market_order=trading_client.submit_order(market_order_data)
+print(market_order)
 
-for trades in trades.data['PLTR']:
-    print(trades)
-    break
-
-11 minuta
+15:30
