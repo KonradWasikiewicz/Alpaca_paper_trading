f1bb69adea694b5fcd7935460262af8b92f71399
KonradWasikiewicz <konrad.wasikiewicz@gmail.com>
2024-10-13 17:19:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-#https://www.youtube.com/watch?v=iF7osyiDkBY
-#API keys
-import config
-# connection to Alpaca
-from alpaca.trading.client import TradingClient
+# #https://www.youtube.com/watch?v=iF7osyiDkBY
+# #API keys
+# import config
+# # connection to Alpaca
+# from alpaca.trading.client import TradingClient
 
-trading_client = TradingClient(config.alpacakey, config.alpacakey_secret)
+# trading_client = TradingClient(config.alpacakey, config.alpacakey_secret)
 
-print(trading_client.get_account().account_number)
-print(trading_client.get_account().buying_power)
+# print(trading_client.get_account().account_number)
+# print(trading_client.get_account().buying_power)
+
+
+from alpaca.data import StockHistoricalDataClient, StockTradesRequest
+from datetime import datetime
+
+data_client = StockHistoricalDataClient("PKQGC4V9XTMJGSD9HF29","2HRWei8pngjyCJmQeceFowiarg6Ktrd2zG9wQ0Qt")
+
+request_params = StockTradesRequest(
+    symbol_or_symbols = "PLTR",
+    start = datetime(2024, 1, 30, 14, 30),
+    end = datetime(2024, 1, 30, 14, 45) 
+)
+
+trades = data_client.get_stock_trades(request_params)
+
+for trades in trades.data['PLTR']:
+    print(trades)
+    break
+
+11 minuta
